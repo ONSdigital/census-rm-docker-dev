54cd6ca2a91c8259171e00f8beab9e73c54e5a93
ryangrundy7 <35301276+ryangrundy7@users.noreply.github.com>
2019-09-05 10:36:28 +0100

    Undelivered as addressed (#30)
    * Added undelivered topics and subscriptions for pubsub
    * Adding rabbit queue definition
    * Add action undelivered queues

--- setup_pubsub.sh
+++ setup_pubsub.sh
@@ -6,6 +6,11 @@ source .env
 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' '$PUBSUB_SETUP_HOST')" != "200" ]]; do sleep 1; done'
 
 curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/$RECEIPT_TOPIC_PROJECT_ID/topics/$RECEIPT_TOPIC_NAME
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/$PPO_UNDELIVERED_SUBSCRIPTION_PROJECT_ID/topics/$PPO_UNDELIVERED_TOPIC_NAME
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/$QM_UNDELIVERED_SUBSCRIPTION_PROJECT_ID/topics/$QM_UNDELIVERED_TOPIC_NAME
 curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/$RECEIPT_TOPIC_PROJECT_ID/subscriptions/$SUBSCRIPTION_NAME -H 'Content-Type: application/json' -d '{"topic": "projects/'$RECEIPT_TOPIC_PROJECT_ID'/topics/'$RECEIPT_TOPIC_NAME'"}'
 curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/$OFFLINE_RECEIPT_TOPIC_PROJECT_ID/topics/$OFFLINE_RECEIPT_TOPIC_NAME
 curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/$OFFLINE_RECEIPT_TOPIC_PROJECT_ID/subscriptions/$OFFLINE_SUBSCRIPTION_NAME -H 'Content-Type: application/json' -d '{"topic": "projects/'$OFFLINE_RECEIPT_TOPIC_PROJECT_ID'/topics/'$OFFLINE_RECEIPT_TOPIC_NAME'"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/$QM_UNDELIVERED_SUBSCRIPTION_PROJECT_ID/subscriptions/$QM_UNDELIVERED_SUBSCRIPTION_NAME -H 'Content-Type: application/json' -d '{"topic": "projects/'$QM_UNDELIVERED_SUBSCRIPTION_PROJECT_ID'/topics/'$QM_UNDELIVERED_TOPIC_NAME'"}'
+curl -X PUT http://$PUBSUB_SETUP_HOST/v1/projects/$PPO_UNDELIVERED_SUBSCRIPTION_PROJECT_ID/subscriptions/$PPO_UNDELIVERED_SUBSCRIPTION_NAME -H 'Content-Type: application/json' -d '{"topic": "projects/'$PPO_UNDELIVERED_SUBSCRIPTION_PROJECT_ID'/topics/'$PPO_UNDELIVERED_TOPIC_NAME'"}'
+
